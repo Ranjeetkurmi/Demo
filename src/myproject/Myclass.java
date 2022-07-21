ad0450776fed482d5754a4459ee681ccb47b2999
ranjkurm <ranjkurm@DVCINH6086658.corp.amdocs.com>
2022-07-21 09:57:34 +0530

    updated Myclass

--- src/myproject/Myclass.java
+++ src/myproject/Myclass.java
@@ -4,5 +4,7 @@ public class Myclass {
  
 	 public static void main(String[] args) {
 		System.out.println("hello from Myclass 2 Thanks");
+		System.out.println("Some change i have made");
+		
 	}
 }
